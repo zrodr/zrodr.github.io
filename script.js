cf869837e96154fb100e725aac78e67f7f45d372
Izaiah Rodriguez <zrodr@protonmail.com>
2023-01-05 18:47:48 -0800

    Beginning development of v2
    - Made preliminary changes: edited color variables, added new logo, code formatting, etc.

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-const carousel = document.getElementById('project-carousel');
-const scrollArrow = document.querySelector('.scroll-arrow');
+const carousel = document.getElementById("project-carousel");
+const scrollArrow = document.querySelector(".scroll-arrow");
 
 const main = () => {
   if (carousel && scrollArrow) {
     // detect if the scrollbar is currently in the left-most or right-most position
-    carousel.addEventListener('scroll', (event) => {
-      if (carousel.scrollWidth - carousel.clientWidth === Math.abs(carousel.scrollLeft)) {
-        setArrowDirection('end');
-      }
-      else if (carousel.scrollLeft === 0) {
-        setArrowDirection('start');
+    carousel.addEventListener("scroll", (event) => {
+      if (
+        carousel.scrollWidth - carousel.clientWidth ===
+        Math.abs(carousel.scrollLeft)
+      ) {
+        setArrowDirection("end");
+      } else if (carousel.scrollLeft === 0) {
+        setArrowDirection("start");
       }
     });
-  
+
     // snap to the left/right card depending on the current arrow direction
-    scrollArrow.addEventListener('click', (event) => {
+    scrollArrow.addEventListener("click", (event) => {
       const currentScrollVal = carousel.scrollLeft;
-      let snapValue = (carousel.scrollWidth / carousel.childElementCount) + 15;
-  
+      let snapValue = carousel.scrollWidth / carousel.childElementCount + 15;
+
       // handles movement from end -> start
-      if (scrollArrow.classList.contains('end')) 
-        snapValue *= -1;
-  
-      carousel.scrollTo(currentScrollVal + snapValue, 0); 
+      if (scrollArrow.classList.contains("end")) snapValue *= -1;
+
+      carousel.scrollTo(currentScrollVal + snapValue, 0);
     });
   }
-}
+};
 
 main();
-
 
 /* helper functions */
 
 function setArrowDirection(position) {
-  if (position === 'start') {
-    scrollArrow.classList.remove('end');
-    scrollArrow.classList.add('start');
-  }
-  else {
-    scrollArrow.classList.remove('start');
-    scrollArrow.classList.add('end');
+  if (position === "start") {
+    scrollArrow.classList.remove("end");
+    scrollArrow.classList.add("start");
+  } else {
+    scrollArrow.classList.remove("start");
+    scrollArrow.classList.add("end");
   }
 }
