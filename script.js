ae53719793c05934359e728f3adc44e5f7154df6
Izaiah Rodriguez <zrodr@protonmail.com>
2023-01-25 16:24:18 -0800

    Initial v2 version completed
    - Added new card to project section
    - Overhauled contact section styling and added contact form
    - (TODO) possible changes pending a cross-browser test

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const carousel = document.getElementById("project-carousel");
 const scrollArrow = document.querySelector(".scroll-arrow");
+const carousel = document.getElementById("project-carousel");
+const carouselElements = carousel.querySelectorAll(".project-card");
+
+const horizontalScrollObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
 
 const main = () => {
   if (carousel && scrollArrow) {
@@ -15,10 +29,15 @@ const main = () => {
       }
     });
 
+    carouselElements.forEach((element) => {
+      horizontalScrollObserver.observe(element);
+    });
+
     // snap to the left/right card depending on the current arrow direction
     scrollArrow.addEventListener("click", (event) => {
       const currentScrollVal = carousel.scrollLeft;
-      let snapValue = carousel.scrollWidth / carousel.childElementCount + 15;
+      let snapValue = carousel.scrollWidth / carousel.childElementCount;
+      snapValue += carousel.scrollWidth % carousel.childElementCount;
 
       // handles movement from end -> start
       if (scrollArrow.classList.contains("end")) snapValue *= -1;
