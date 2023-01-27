3bb3edb60fada9eb3bcec5bbcafa69dca472d8a9
Izaiah Rodriguez <zrodr@protonmail.com>
2023-01-26 16:47:40 -0800

    Continued tweaks to v2 layout
    - Added fade-in effect
    - Carousel cards are now centered on desktop & more appropriately sized
    - Added simple footer with a navigate to top link

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const scrollArrow = document.querySelector(".scroll-arrow");
-const carousel = document.getElementById("project-carousel");
-const carouselElements = carousel.querySelectorAll(".project-card");
+const fadeInElements = document.querySelectorAll(".hidden");
+const projectCarouselElements = document
+  .getElementById("project-carousel")
+  .querySelectorAll(".project-card");
 
 const horizontalScrollObserver = new IntersectionObserver(
   (entries) => {
@@ -15,48 +16,26 @@ const horizontalScrollObserver = new IntersectionObserver(
   { threshold: 0.5 }
 );
 
-const main = () => {
-  if (carousel && scrollArrow) {
-    // detect if the scrollbar is currently in the left-most or right-most position
-    carousel.addEventListener("scroll", (event) => {
-      if (
-        carousel.scrollWidth - carousel.clientWidth ===
-        Math.abs(carousel.scrollLeft)
-      ) {
-        setArrowDirection("end");
-      } else if (carousel.scrollLeft === 0) {
-        setArrowDirection("start");
-      }
-    });
+const fadeInObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("fade-in");
+    } else {
+      entry.target.classList.remove("fade-in");
+    }
+  });
+});
 
-    carouselElements.forEach((element) => {
+const main = () => {
+  if (projectCarouselElements && fadeInElements) {
+    projectCarouselElements.forEach((element) => {
       horizontalScrollObserver.observe(element);
     });
 
-    // snap to the left/right card depending on the current arrow direction
-    scrollArrow.addEventListener("click", (event) => {
-      const currentScrollVal = carousel.scrollLeft;
-      let snapValue = carousel.scrollWidth / carousel.childElementCount;
-      snapValue += carousel.scrollWidth % carousel.childElementCount;
-
-      // handles movement from end -> start
-      if (scrollArrow.classList.contains("end")) snapValue *= -1;
-
-      carousel.scrollTo(currentScrollVal + snapValue, 0);
+    fadeInElements.forEach((element) => {
+      fadeInObserver.observe(element);
     });
   }
 };
 
 main();
-
-/* helper functions */
-
-function setArrowDirection(position) {
-  if (position === "start") {
-    scrollArrow.classList.remove("end");
-    scrollArrow.classList.add("start");
-  } else {
-    scrollArrow.classList.remove("start");
-    scrollArrow.classList.add("end");
-  }
-}
